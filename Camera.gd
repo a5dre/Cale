d445c9de44d026027bb1c286d531e54a3daf7a1d
perkcaan <97648908+perkcaan@users.noreply.github.com>
2022-04-21 14:17:50 -0400

    New Level One layout

--- Camera.gd
+++ Camera.gd
@@ -3,11 +3,6 @@ extends Camera2D
 var player =null
 
 func _process(_delta):
-	if player == null:
 		player = get_node_or_null("/root/Game/Player_Container/Player")
-	else:
-<<<<<<< Updated upstream
-				position = player.position
-=======
-ww		position = player.position
->>>>>>> Stashed changes
+		if player != null:
+			position = player.position
